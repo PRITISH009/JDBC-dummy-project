ced10b3c711095f8d9bc4fcf80b114d67cce569f
PRITISH009 <pritishgupta009@gmail.com>
2020-07-10 20:27:19 +0530

    Update Employee.java

--- JDBCWork/src/main/java/com/company/eis/bean/Employee.java
+++ JDBCWork/src/main/java/com/company/eis/bean/Employee.java
@@ -5,6 +5,7 @@ public class Employee {
 	private String name;
 	private int salary;
 	private String designation;
+	private String insuranceScheme;
 	
 	public String getName() {
 		return name;
@@ -29,6 +30,12 @@ public class Employee {
 	}
 	public void setDesignation(String designation) {
 		this.designation = designation;
+	}
+	public String getInsuranceScheme(){
+		return insuranceScheme;
+	}
+	public void setInsuranceScheme(String insuranceScheme){
+		this.insuranceScheme = insuranceScheme;
 	}
 	
 }
